763eb0077fe400877116b850cf6f73b2991a8ef3
yuecx <yuecx2004@163.com>
2026-01-15 16:54:23 +0800

    init

---
+++ src/main/java/com/yue/yueaiagent/YueAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.yue.yueaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YueAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YueAiAgentApplication.class, args);
+    }
+
+}
